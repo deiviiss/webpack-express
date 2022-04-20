d581c6c3424d30d74c4f98572e0a26c158b09b3e
David Hilera <david.hilera@hotmail.com>
2022-04-19 21:48:12 -0500

    [deploy] Server Express-Webpack

---
+++ src/index.ts
@@ -0,0 +1,75 @@
+// const express = require('express');
+import * as express from "express";
+import { Request, Response } from "express";
+
+const app = express();
+
+const port = process.env.PORT || 3005;
+
+const initialState = {
+  cart: [],
+  products: [
+    {
+      id: "1",
+      image: "https://arepa.s3.amazonaws.com/camiseta.png",
+      title: "Camiseta",
+      price: 25,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      id: "3",
+      image: "https://arepa.s3.amazonaws.com/mug.png",
+      title: "Mug",
+      price: 10,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      id: "4",
+      image: "https://arepa.s3.amazonaws.com/pin.png",
+      title: "Pin",
+      price: 4,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      id: "5",
+      image: "https://arepa.s3.amazonaws.com/stickers1.png",
+      title: "Stickers",
+      price: 2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      id: "6",
+      image: "https://arepa.s3.amazonaws.com/stickers2.png",
+      title: "Stickers",
+      price: 2,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+    {
+      id: "7",
+      image: "https://arepa.s3.amazonaws.com/hoodie.png",
+      title: "Hoodie",
+      price: 35,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    },
+  ],
+};
+
+//router
+// app.get('/', (req, res) => {
+//   res.send('Hello wordl')
+// })
+
+//typescript
+app.get("/", (req: Request, res: Response) => {
+  res.send("API V1");
+});
+
+// get products
+app.get("/api/v1/products", (req: Request, res: Response) => {
+  res.json(initialState);
+});
+
+//server on
+app.listen(port, () => {
+  console.log("Server on");
+});
